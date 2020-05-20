411e7767eea07569d34c133ed39467205577cacb
ioedumac <eacevedof@yahoo.es>
2020-05-20 12:42:55 +0200

    app cambiado a components/boot

--- frontend_react/pannel/src/index.js
+++ frontend_react/pannel/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './components/boot/app.css';
-import * as serviceWorker from './serviceWorker';
 import App from "./components/boot/App";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
