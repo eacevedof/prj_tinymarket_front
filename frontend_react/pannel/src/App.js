7dbb1f0a1ef3590f855ce79f0f2c7a0029b065ad
ioedumac <eacevedof@yahoo.es>
2020-05-20 11:30:54 +0200

    he restructurado los componentes creando boot. Da error pero es por lainicializacion de las globales en boot

--- frontend_react/pannel/src/App.js
+++ frontend_react/pannel/src/App.js
@@ -1,62 +1,13 @@
 //App.js
-import React, {useContext, useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  //Link,
-  //useRouteMatch,
-  //useParams
-} from "react-router-dom";
-import GlobalProvider, {GlobalContext} from './components/context/global_context';
-import ProductList from './components/product/product_list';
-import OrderDetail from "./components/order/order_detail"
-import objorder from "./models/order"
-
-import LocalDb from "./helpers/local_db"
-import E404 from "./components/errors/404/e404"
-import _ from "lodash"
+import React from 'react';
+import GlobalProvider from './components/context/global_context';
+import Boot from './components/boot/boot';
 import "./index.css"
 
-
 function App(){
-
-  //const {order, set_order, search, set_search} = useContext(GlobalContext)
-
-  const [order, set_order] = useState(objorder)
-  const [search, set_search] = useState("")
-
-  console.log("App.order ",order)
-
-  useEffect(() => {
-    console.log("app.useffect.search",search)
-    if(order.products.length === 0){
-      const dborder = LocalDb.select("order")
-      if(!_.isEmpty(dborder)){
-        console.log("app.useeffect.dborder ...loading from localstorage",dborder)
-        set_order(dborder)
-      }
-    }
-  }, []);
-
   return (
     <GlobalProvider>
-      <Router>
-        <Switch>
-
-          <Route exact path="/">
-            <ProductList order={order} set_order={set_order} search={search} set_search={set_search}/>
-          </Route>        
-          
-          <Route path="/order">
-            <OrderDetail order={order} set_order={set_order} search={search}/>
-          </Route>
-                      
-          <Route path="*" status={404}>
-            <E404/>
-          </Route>
-        </Switch>
-      </Router>
+      <Boot/>
     </GlobalProvider>
   );
 }//App
---
+++ frontend_react/pannel/src/components/boot/boot.js
@@ -0,0 +1,63 @@
+import React, {useContext, useEffect} from 'react';
+import {GlobalContext} from '../context/global_context';
+
+import ProductList from '../product/product_list';
+import OrderDetail from "../order/order_detail"
+import objorder from "../../models/order"
+
+import LocalDb from "../../helpers/local_db"
+import E404 from "../errors/404/e404"
+import _ from "lodash"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  //Link,
+  //useRouteMatch,
+  //useParams
+} from "react-router-dom";
+
+function Boot() {
+
+  const {order, set_order, search, set_search} = useContext(GlobalContext)
+
+  //const [order, set_order] = useState(objorder)
+  //const [search, set_search] = useState("")
+
+  console.log("App.order ",order)
+
+  useEffect(() => {
+    console.log("app.useffect.search",search)
+    if(order.products.length === 0){
+      const dborder = LocalDb.select("order")
+      if(!_.isEmpty(dborder)){
+        console.log("app.useeffect.dborder ...loading from localstorage",dborder)
+        set_order(dborder)
+      }
+    }
+  }, []);
+
+
+  return (
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <ProductList order={order} set_order={set_order} search={search} set_search={set_search}/>
+        </Route>        
+        
+        <Route path="/order">
+          <OrderDetail order={order} set_order={set_order} search={search}/>
+        </Route>
+                    
+        <Route path="*" status={404}>
+          <E404/>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+
+export default Boot;
