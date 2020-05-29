bd978ef82f776ff54c7d362f86bf1a9e0cc10f89
ioedumac <eacevedof@yahoo.es>
2020-05-29 12:03:03 +0200

    version en footer

--- frontend_react/pannel/src/components/common/footer.js
+++ frontend_react/pannel/src/components/common/footer.js
@@ -6,8 +6,11 @@ function footer() {
       <div className="container-fluid">
         <nav>
           <p className="copyright text-center">
-              © {new Date().getFullYear()} 
-              <a href="http://www.elchalanaruba.com" target="_blank" rel="noopener noreferrer"> El Chalán Aruba</a>
+            © {new Date().getFullYear()} 
+            <a href="http://www.elchalanaruba.com" target="_blank" rel="noopener noreferrer"> El Chalán Aruba</a>
+          </p>
+          <p>
+            Version 1.0.0
           </p>
         </nav>
       </div>
