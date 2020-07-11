49cd03ad27bfc326a8a13dc5d9dd55a5b728406c
ioedumac <eacevedof@yahoo.es>
2020-07-11 14:01:47 +0200

    configurando check en actions

--- frontend_react/restrict/src/helpers/bootstrap/tableaction/tdaction.js
+++ frontend_react/restrict/src/helpers/bootstrap/tableaction/tdaction.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import shortid from "shortid"
 
 function Tdaction({araction}) {
+  const id = shortid.generate()
+  const strid = `dd-${id}`
+
   return (
     <td>
       <div className="dropdown">
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-expanded="false">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+          <label className="form-check-label" for="flexCheckDefault"></label>
+        </div>
+        <button className="btn btn-secondary dropdown-toggle" type="button" id={strid} data-toggle="dropdown" aria-expanded="false">
           Action
         </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+        <ul className="dropdown-menu" aria-labelledby={strid}>
           <li><button className="dropdown-item" type="button">Update</button></li>
           <li><button className="dropdown-item" type="button">Delete</button></li>
           <li><button className="dropdown-item" type="button">Delete L</button></li>
