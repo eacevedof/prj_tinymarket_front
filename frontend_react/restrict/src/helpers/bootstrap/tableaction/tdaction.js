12b2527689fb422f2d521f63ede271c0265c4444
ioedumac <eacevedof@yahoo.es>
2020-07-11 14:03:27 +0200

    check alineado en actions

--- frontend_react/restrict/src/helpers/bootstrap/tableaction/tdaction.js
+++ frontend_react/restrict/src/helpers/bootstrap/tableaction/tdaction.js
@@ -11,10 +11,10 @@ function Tdaction({araction}) {
         <div className="form-check">
           <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
           <label className="form-check-label" for="flexCheckDefault"></label>
+          <button className="btn btn-secondary dropdown-toggle" type="button" id={strid} data-toggle="dropdown" aria-expanded="false">
+            Action
+          </button>          
         </div>
-        <button className="btn btn-secondary dropdown-toggle" type="button" id={strid} data-toggle="dropdown" aria-expanded="false">
-          Action
-        </button>
         <ul className="dropdown-menu" aria-labelledby={strid}>
           <li><button className="dropdown-item" type="button">Update</button></li>
           <li><button className="dropdown-item" type="button">Delete</button></li>
