ba7dc86e3d6f16cdc53fc14d99bf4ef0ec5c655a
ioedumac <eacevedof@yahoo.es>
2020-07-11 17:48:31 +0200

    restrict: routesproduct esqueleto

--- frontend_react/restrict/src/modules/product/routes.js
+++ frontend_react/restrict/src/modules/product/routes.js
@@ -5,9 +5,26 @@ import ProductIndex from "./index"
 
 function RoutesProduct() {
   return (
+    <>
     <Route path="/admin/product">
       <ProductIndex />
     </Route>
+    <Route exact path="/admin/product/:id">
+      <>prod id</>
+    </Route>
+    <Route exact path="/admin/product/update/:id">
+      <>update</>
+    </Route>
+    <Route exact path="/admin/product/delete/:id">
+      <>deletex</>
+    </Route>
+    <Route exact path="/admin/product/delete-logic/:id">
+      <>delete logic</>
+    </Route>    
+    <Route exact path="/admin/product/clone/:id">
+      <>clone</>
+    </Route>        
+    </>
   )
 }
 
