4f589524bb6d7d73bef6fd5e3b377ee668bfdf83
ioedumac <eacevedof@yahoo.es>
2020-07-11 14:07:47 +0200

    pinta el check pero el desplegable click da error

--- frontend_react/restrict/src/helpers/bootstrap/tableaction/tdaction.js
+++ frontend_react/restrict/src/helpers/bootstrap/tableaction/tdaction.js
@@ -4,16 +4,17 @@ import shortid from "shortid"
 function Tdaction({araction}) {
   const id = shortid.generate()
   const strid = `dd-${id}`
+  const chkid = `chk-${id}`
 
   return (
     <td>
       <div className="dropdown">
         <div className="form-check">
-          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-          <label className="form-check-label" for="flexCheckDefault"></label>
+          <input className="form-check-input" type="checkbox" id={chkid} />
+          <label className="form-check-label" htmlFor={chkid}></label>
           <button className="btn btn-secondary dropdown-toggle" type="button" id={strid} data-toggle="dropdown" aria-expanded="false">
             Action
-          </button>          
+          </button>
         </div>
         <ul className="dropdown-menu" aria-labelledby={strid}>
           <li><button className="dropdown-item" type="button">Update</button></li>
