e391720f5afc624cafdb4dd9fc62b677db4d7a9e
ioedumac <eacevedof@yahoo.es>
2020-07-11 11:22:02 +0200

    head ok

--- frontend_react/restrict/src/helpers/bootstrap/tablehead.js
+++ frontend_react/restrict/src/helpers/bootstrap/tablehead.js
@@ -16,19 +16,11 @@ function TableHead({arhead}) {
         <th key={i} scope="col">{objth.text}</th>
     )
   })// get_tds
-  
-  let tds = (<th>-</th>)
 
-  console.log("arhead",arhead,"length:",arhead.length,", isarray:",Array.isArray(arhead))
-  if(Array.isArray(arhead)){
-    tds = get_tds(arhead)
-    console.log("tds:",tds)
-  }
-  
   return (
     <thead>
       <tr>
-      {tds}
+      {get_tds(arhead)}
       </tr>
     </thead>
   )
