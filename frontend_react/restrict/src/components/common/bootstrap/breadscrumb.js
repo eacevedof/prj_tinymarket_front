c372ad3c9f131bfb2e3fce105eebee418d31e8fa
ioedumac <eacevedof@yahoo.es>
2020-07-12 11:07:50 +0200

    breadscrumb with get_lis

--- frontend_react/restrict/src/components/common/bootstrap/breadscrumb.js
+++ frontend_react/restrict/src/components/common/bootstrap/breadscrumb.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 
 //import {GlobalContext} from '../../context/global_context';
-function Breadscrumb() {
+function Breadscrumb({arobjs}) {
+
+  const get_lis = arobjs => arobjs.map((objli, i) => {
+    return (
+      <li className="breadcrumb-item">
+        <NavLink exact to={objli.path}>
+          {objli.text}
+        </NavLink>       
+      </li>
+    )
+  })
+
   return (
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
