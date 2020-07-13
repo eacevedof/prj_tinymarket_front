5b2e7d2f7c8b8cf1f09a1241cf89b3f260b31b3b
ioedumac <eacevedof@yahoo.es>
2020-07-13 20:15:35 +0200

    devuelve el registro

--- frontend_react/restrict/src/modules/product/fetchers/fetch_update.js
+++ frontend_react/restrict/src/modules/product/fetchers/fetch_update.js
@@ -2,9 +2,11 @@ import apidb from "../../../providers/apidb"
 import {get_obj_entity} from "../queries/queries_entity"
 
 export const async_get_by_id = async (id) => {
+  //alert("id:"+id)
   if(!id) return []
-  const objparam = {filters:{id}}
+  const objparam = {filters:{fields:[{field:"id",value:id}]}}
   const query = get_obj_entity(objparam)
   const r = await apidb.async_get_list(query)
+  if(r.length>0) return r[0]
   return r
 }
