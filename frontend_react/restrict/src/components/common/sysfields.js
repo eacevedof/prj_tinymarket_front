cd501850e128ec0b4d9af8a37bd0be7772cedd04
ioedumac <eacevedof@yahoo.es>
2020-07-25 14:43:47 +0200

    restrict: ahora no se ven los datos del form

--- frontend_react/restrict/src/components/common/sysfields.js
+++ frontend_react/restrict/src/components/common/sysfields.js
@@ -3,30 +3,35 @@ import async_get_useralias from "components/common/sysfields"
 import {is_defined, pr} from "helpers/functions"
 
 function Sysfields({sysdata}){
-    
+  pr("sysfields:")
+  pr(sysdata)
+
   const objdefault = {
-    insert_date: "",
-    insert_user: "",
-    update_date: "",
-    update_user: "",
-    delete_date: "",
-    delete_user: "",
+    insert_date: "a",
+    insert_user: "b",
+    update_date: "c",
+    update_user: "d",
+    delete_date: "e",
+    delete_user: "f",
   }
 
-  const [objsys, set_objsys] = useState({...objdefault, ...sysdata})
+  const [objsys, set_objsys] = useState({...objdefault})
 
   const async_onload = async () => {
+    //pr(objdefault)
     //pr(sysdata)
     //const objsysdata = {...sysdata}
     //pr(objsysdata)
-    //const id = objsysdata.insert_user
+    //const id = objobjsys.insert_user
     //const insert_user = await async_get_useralias(objdefault.insert_user)
-    //const insert_user = await async_get_useralias(objsysdata.insert_user)
-    //const update_user = await async_get_useralias(objsysdata.update_user)
-    //const delete_user = await async_get_useralias(objsysdata.delete_user)
+    //const insert_user = await async_get_useralias(objobjsys.insert_user)
+    //const update_user = await async_get_useralias(objobjsys.update_user)
+    //const delete_user = await async_get_useralias(objobjsys.delete_user)
 
     //const temp = {...objsys, insert_user, update_user, delete_user}
-    //set_objsys(temp)
+    set_objsys({...objdefault, ...sysdata})
+    pr("objsys to async_onload")
+    pr(objsys)
   }
 
   useEffect(()=>{
@@ -39,24 +44,24 @@ function Sysfields({sysdata}){
       <h4>sysfields</h4>
       <div className="row">
         <div className="col-3">Created at:</div>
-        <div className="col-3">{sysdata.insert_date}</div>
+        <div className="col-3">{objsys.insert_date}</div>
         <div className="col-3">Created by:</div>
-        <div className="col-3">{sysdata.insert_user}</div>              
+        <div className="col-3">{objsys.insert_user}</div>              
       </div>
       <div className="row">
         <div className="col-3">Modified:</div>
-        <div className="col-3">{sysdata.update_date}</div>
+        <div className="col-3">{objsys.update_date}</div>
         <div className="col-3">Modified by:</div>
-        <div className="col-3">{sysdata.update_user}</div>                        
+        <div className="col-3">{objsys.update_user}</div>                        
       </div>
       {
-        is_defined(sysdata.delete_date) && sysdata.delete_date!=null ?
+        is_defined(objsys.delete_date) && objsys.delete_date!=null && objsys.delete_date!=="" ?
         (
           <div className="row">
             <div className="col-3">Delete:</div>
-            <div className="col-3">{sysdata.delete_date}</div>
+            <div className="col-3">{objsys.delete_date}</div>
             <div className="col-3">Delete by:</div>
-            <div className="col-3">{sysdata.delete_user}</div>                        
+            <div className="col-3">{objsys.delete_user}</div>                        
           </div>          
         ): null
       }
