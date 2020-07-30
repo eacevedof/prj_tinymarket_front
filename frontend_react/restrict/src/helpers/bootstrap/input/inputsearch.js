f6fb952d222624dfb4c0883a216d7a1f555cd954
ioedumac <eacevedof@yahoo.es>
2020-07-30 13:46:21 +0200

    restirct inputsearch mejora

--- frontend_react/restrict/src/helpers/bootstrap/input/inputsearch.js
+++ frontend_react/restrict/src/helpers/bootstrap/input/inputsearch.js
@@ -1,18 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
-function InputSearch({arhead}) {
+function InputSearch(){
+
+  const [search, set_search] = useState("")
 
   useEffect(()=>{
     return ()=> console.log("inputsearch unmounting")
   },[])
 
   return (
-    <form>
-      <div className="mb-3">
-        <input type="email" className="form-control" aria-describedby="search" placeholder="filter" />
-        <div id="txt-search" className="form-text">
+    <form className="row g-3">
+      <div className="col-8">
+        <input type="text" className="form-control" aria-describedby="search" placeholder="filter" />
+        <div className="form-text">
           se puede usar como comandos
         </div>
+      </div>
+      <div className="col-4">
+        <button type="submit" className="btn btn-primary">search</button>
       </div>
     </form>
   )
