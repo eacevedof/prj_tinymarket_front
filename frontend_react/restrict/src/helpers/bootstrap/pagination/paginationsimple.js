23c7d22aa0c3a2187a54f16708dfa1c653efd4b6
ioedumac <eacevedof@yahoo.es>
2020-07-30 14:06:17 +0200

    restirct paginacion no va con navlink

--- frontend_react/restrict/src/helpers/bootstrap/pagination/paginationsimple.js
+++ frontend_react/restrict/src/helpers/bootstrap/pagination/paginationsimple.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { NavLink } from "react-router-dom";
 
 function PaginationSimple({page,objconf}){
 
@@ -16,19 +17,31 @@ function PaginationSimple({page,objconf}){
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
+
         <li className="page-item">
           <a className="page-link" href="#" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
-        <li className="page-item"><a className="page-link" href="#">1</a></li>
-        <li className="page-item"><a className="page-link" href="#">2</a></li>
-        <li className="page-item"><a className="page-link" href="#">3</a></li>
+
+        <li className="page-item">
+          <NavLink className="page-link" exact to={"/admin/products/1"}>1</NavLink>
+        </li>
+
+        <li className="page-item">
+          <NavLink className="page-link" exact to={"/admin/products/2"}>2</NavLink>
+        </li>
+        
+        <li className="page-item">
+          <NavLink className="page-link" exact to={"/admin/products/3"}>3</NavLink>
+        </li>
+        
         <li className="page-item">
           <a className="page-link" href="#" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
           </a>
         </li>
+
       </ul>
     </nav>
   )
