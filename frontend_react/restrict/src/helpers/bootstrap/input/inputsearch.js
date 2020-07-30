40d1b7fa5df6915d6fa4c6402a1299585fb33060
ioedumac <eacevedof@yahoo.es>
2020-07-30 15:02:47 +0200

    importo procesos de formulario insert

--- frontend_react/restrict/src/helpers/bootstrap/input/inputsearch.js
+++ frontend_react/restrict/src/helpers/bootstrap/input/inputsearch.js
@@ -1,24 +1,74 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
 import SubmitAsync from 'helpers/bootstrap/button/submitasync';
 
 function InputSearch(){
 
-  const [search, set_search] = useState("")
   const [issubmitting, set_issubmitting] = useState(false)
+  const [error, set_error] = useState("")
+  const [success, set_success] = useState("")
+  const refcode = useRef(null)
 
-  const on_submit = (evt)=>{
-  
+  const formdefault = {
+    search: ""
   }
 
+  const [formdata, set_formdata] = useState({...formdefault})
+
+  const get_id = elem => {
+    const idpref = elem.id || ""
+    const parts = idpref.split("-")
+    //console.log("parts",parts)
+    if(parts.length>1) return parts[1]
+    //console.log("elem.idpref",idpref)
+    return idpref
+  }
+
+  const updateform = evt =>{
+    const elem = evt.target
+    
+    set_formdata({search:""})
+
+    console.log("updateform.formdata",formdata)
+    //console.log("updateform.formdata",formdata,"formdata.url_image",formdata.url_image)
+  }
+
+  const before_submit = () => {}
+
+  const on_submit = async (evt)=>{
+    console.log("product.insert.on_submit.formdata:",formdata)
+    evt.preventDefault()
+
+    set_issubmitting(true)
+    set_error("")
+    set_success("")
+
+    //hacer insert y enviar fichero
+    before_submit()
+    try {
+
+    } 
+    catch (error) {
+      console.log("error:",error.toString())
+      set_error(error.toString())
+    } 
+    finally {
+      set_issubmitting(false)
+    }
+    
+  }// on_submit
   useEffect(()=>{
     return ()=> console.log("inputsearch unmounting")
   },[])
 
   return (
-    <form className="row">
+    <form className="row" onSubmit={on_submit}>
       <div className="col-8">
-        <input type="text" className="form-control" aria-describedby="search" placeholder="filter" />
+        <input type="text" className="form-control" aria-describedby="search" placeholder="filter" 
+          
+          value={formdata.search}
+          onChange={updateform}        
+        />
         <div className="form-text">
           se puede usar como comandos
         </div>
