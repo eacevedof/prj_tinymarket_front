235cdc9430e64dee004d80267929f3e6adc43bd6
ioedumac <eacevedof@yahoo.es>
2020-07-30 12:38:05 +0200

    mejoro filter get sin query

--- frontend_react/restrict/src/helpers/filter.js
+++ frontend_react/restrict/src/helpers/filter.js
@@ -1,26 +1,23 @@
 import {pr, get_urlvalue, is_defined, is_empty} from "helpers/functions"
-import { map } from "lodash"
 
-//const get_fields = confs => confs.map(conf =>  conf.fields.map( objf => ({name:`${conf.alias}.${objf.name}`,labels:objf.labels})) )  
 const get_fields = confs => confs.
-                                    filter(objconf => !is_empty(objconf.table) ).//map(objconf =>  objconf.table.alias)  
-                                    //map(objconf => pr(objconf.fields,"FFF"))
-                                    map(objconf => objconf.table.fields.map(objf => ({name:`${objconf.table.alias}.${objf.name}`,labels:objf.labels}))).
-                                    reduce((arac, arcurr) => [...arac, ...arcurr])
+                              filter(objconf => !is_empty(objconf.table) ).
+                              map(objconf => objconf.table.fields.map(objf => ({name:`${objconf.table.alias}.${objf.name}`,labels:objf.labels}))).
+                              reduce((arac, arcurr) => [...arac, ...arcurr])
                                     
 const get_values = arlabels => arlabels.map(strlabel => get_urlvalue(strlabel)).filter(strval => strval!="")[0] || ""
 
 const get_fields_vals = arfields => {
-  const arvalues = arfields.map(objfield => ({name:objfield.name, value:get_values(objfield.labels)}))
+  const arvalues = arfields.map(objfield => ({name:objfield.name, value:get_values(objfield.labels) }))
   return arvalues
 }
 
-const filterget = (confs, query) => {
+const filterget = (confs) => {
   //pr(confs);return;
   const arfields = get_fields(confs)
   const arvalues = get_fields_vals(arfields)
-
-  return arvalues
+  const filters = arvalues.filter(obj => obj.value!="").map(obj => `${obj.name} LIKE '%${obj.value}%'`)
+  return filters
   //devuelve un array de condiciones
 }
 
