32b9bb0fb8258d68592e4fc7c5b56bf2dba7f50c
ioedumac <eacevedof@yahoo.es>
2020-08-04 21:50:02 +0200

    constants con url upload

--- frontend_react/restrict/src/config/constants.js
+++ frontend_react/restrict/src/config/constants.js
@@ -6,12 +6,12 @@ export const APP_VERSION = "v1.0.0.beta.1.build"
 export const BASE_URL = "http://localhost:10000"
 export const CONTEXT = "c4"
 export const DB_NAME = "db-tinymarket"
-export const URL_API_UPLOAD = "http://localhost:4000"
+export const URL_API_UPLOAD = "http://localhost:4000/upload"
 /**/
 
 /**/
 export const BASE_URL = "https://dbsapify.theframework.es"
 export const CONTEXT = "c4"
 export const DB_NAME = "db-tinymarket"
-export const URL_API_UPLOAD = "https://upload.theframework.es"
+export const URL_API_UPLOAD = "https://upload.theframework.es/upload"
 /***/
