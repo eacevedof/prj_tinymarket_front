09a51ab13e8d1b1ffa8a386379b847d961d47c16
ioedumac <eacevedof@yahoo.es>
2020-08-05 18:10:27 +0200

    backup get_filtercmd no devuelve lo que necesito

--- frontend_react/restrict/src/helpers/filter.js
+++ frontend_react/restrict/src/helpers/filter.js
@@ -6,10 +6,10 @@ const get_fields = arconfs => arconfs.
                               map(objconf => objconf.table.fields.map(objf => ({name:`${objconf.table.alias}.${objf.name}`,labels:objf.labels}))).
                               reduce((arac, arcurr) => [...arac, ...arcurr])
                                     
-const get_values = arlabels => arlabels.map(strlabel => get_urlvalue(strlabel)).filter(strval => strval!=="")[0] || ""
+const get_value = arlabels => arlabels.map(strlabel => get_urlvalue(strlabel)).filter(strval => strval!=="")[0] || ""
 
 const get_fields_vals = arfields => {
-  const arvalues = arfields.map(objfield => ({name:objfield.name, value:get_values(objfield.labels) }))
+  const arvalues = arfields.map(objfield => ({name:objfield.name, value:get_value(objfield.labels) }))
   return arvalues
 }
 
@@ -27,20 +27,35 @@ const get_filterand = arfilterconf => {
   return objfilter
 }
 
+const CMD_SEPARATOR = ";"
+const get_cmdkeyval = strkey => strkey.split(CMD_SEPARATOR).
+                                  map(strkv => strkv.trim()).
+                                  map(strkv => strkv.split(CMD_SEPARATOR)).
+                                  filter(ar => ar.length===2).
+                                  map(ar => ({label:ar[0].trim().toLowerCase(),value:ar[1].trim()}))
 
-const get_cmdkeyval = strkey => strkey.split(";").map(strkv => strkv.trim()).map(strkv => strkv.split(":"))
+//const get_valuebylabels = arlabels => arlabels.map(strlabel => get_urlvalue(strlabel)).filter(strval => strval!=="")[0] || ""
 
+const get_value_by_label = (arlblsval, label) => arlblsval.filter(objlblval => objlblval.label === label).map(objlblval => objlblval.value)[0] || ""
+
+const get_value_by_labels = (arlabels, arlblsval) => arlabels.map(label => get_value_by_label(arlblsval, label))
+
+const get_mergedbylabel = (arfieldlbls, arlblval) => arfieldlbls.map(objflbls => ({name:objflbls.name, value:get_value_by_labels(objflbls.labels, arlblval)}))
 
 export const get_filtercmd = (arfilterconf, search) => {
   if(!search) return {}
   const cmd =  search.replace(">","").trim()
-  const kval = get_cmdkeyval(cmd)
-  pr(kval,"kval")
-  const arfields = get_fields(arfilterconf)
+  
+  const arfieldlabels = get_fields(arfilterconf)  
+  const arlblval = get_cmdkeyval(cmd)
 
+  const x = get_mergedbylabel(arfieldlabels, arlblval)
+  pr(x,"x");return {}
+
+  const arvalues = []
   const objfilter = {
     op: "OR",
-    fields: arfields.map(obj => ({field:obj.name, value:search})),
+    fields: arvalues.filter(obj => obj.value !== "").map(obj => ({field:obj.name, value:obj.value})),
   }   
   return objfilter
 }
