278e86c232645911c7f140a244ba54753f5434e8
ioedumac <eacevedof@yahoo.es>
2020-08-11 17:17:32 +0200

    posicion dropdown

--- frontend_react/restrict/src/helpers/bootstrap/dropdown/dropdowntable.js
+++ frontend_react/restrict/src/helpers/bootstrap/dropdown/dropdowntable.js
@@ -12,17 +12,21 @@ function DropdownTable({multiconf}) {
 
   return (
     <div className="row">
-      <button type="button" className="btn btn-secondary dropdown-toggle"  id="grid-ulmultiaction" data-toggle="dropdown" aria-expanded="false">
-        {selected}
-      </button>          
-      <ul className="dropdown-menu" aria-labelledby="grid-ulmultiaction">
-        {Object.keys(multiconf.ACTIONS).map(action => (
-            <li key={get_uuid()}>
-              <button type="button" className="dropdown-item" value={multiconf.ACTIONS[action]} onClick={on_click}>{multiconf.ACTIONS[action]}</button>
-            </li>
-          ))}
-      </ul>
-      <button type="button" className="btn btn-success"><i className="fa fa-check-circle" aria-hidden="true"></i></button>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary dropdown-toggle"  id="grid-ulmultiaction" data-toggle="dropdown" aria-expanded="false">
+          {selected}
+        </button>          
+        <ul className="dropdown-menu" aria-labelledby="grid-ulmultiaction">
+          {Object.keys(multiconf.ACTIONS).map(action => (
+              <li key={get_uuid()}>
+                <button type="button" className="dropdown-item" value={multiconf.ACTIONS[action]} onClick={on_click}>{multiconf.ACTIONS[action]}</button>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="col-2 float-left pl-0">
+        <button type="button" className="btn btn-success"><i className="fa fa-check-circle" aria-hidden="true"></i></button>
+      </div>
     </div>
   )
 }
