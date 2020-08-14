d0439b4bd0d1f01c7e939db08ae5069045f725ef
ioedumac <eacevedof@yahoo.es>
2020-08-14 20:41:47 +0200

    confirm button segun accion selecionada

--- frontend_react/restrict/src/helpers/bootstrap/dropdown/dropdowntable.js
+++ frontend_react/restrict/src/helpers/bootstrap/dropdown/dropdowntable.js
@@ -16,6 +16,17 @@ function DropdownTable({multiconf, fnconfirm}) {
     fnconfirm(selected)
   }
   
+  const get_btnconfirm = () => {
+    if(selected==="") return null
+    return (
+      <div className="col-3 float-left pl-0">
+        <button type="button" className="btn btn-danger" onClick={on_confirm}>
+          <i className="fa fa-check-circle" aria-hidden="true"></i> confirm
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className="row">
       <div className="col-3">
@@ -32,17 +43,15 @@ function DropdownTable({multiconf, fnconfirm}) {
                   value={action}
                   text={multiconf.ACTIONS[action]["text"]}
                   onClick={on_click}
-                  
+
                   >{multiconf.ACTIONS[action]["text"]}</button>
               </li>
             ))}
         </ul>
       </div>
-      <div className="col-2 float-left pl-0">
-        <button type="button" className="btn btn-success" onClick={on_confirm}>
-          <i className="fa fa-check-circle" aria-hidden="true"></i>
-        </button>
-      </div>
+      {
+        get_btnconfirm()
+      }
     </div>
   )
 }
